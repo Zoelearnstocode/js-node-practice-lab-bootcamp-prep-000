67e9dbfc0932ced96d61fcd3b8ffd31ff1af214b
Zoe  Allen <zca@live.co.uk>
2018-02-17 20:19:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // write your code below!
 
 var name = "zoe"
-var height = "168"
+var height = 168
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
 console.log(height)
+console.log(parseInt(height) + 1 )
